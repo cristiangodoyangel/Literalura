5ad00539206b3fe73c3684b853370a0ad0c93e64
Star <creacciones@gmail.com>
2024-12-01 01:54:40 -0300

    - se agrega consulta a API 10 libros mas descargados.

--- src/main/java/com/aluracursos/desafio/principal/Principal.java
+++ src/main/java/com/aluracursos/desafio/principal/Principal.java
@@ -1,8 +1,11 @@
 package com.aluracursos.desafio.principal;
 
 import com.aluracursos.desafio.model.Datos;
+import com.aluracursos.desafio.model.DatosLibros;
 import com.aluracursos.desafio.service.ConsumoAPI;
 import com.aluracursos.desafio.service.ConvierteDatos;
+
+import java.util.Comparator;
 
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
@@ -14,6 +17,15 @@ public class Principal {
         var json = consumoAPI.obtenerDatos(URL_BASE);
         System.out.println(json);
         var datos =conversor.obtenerDatos(json, Datos.class);
+        System.out.println(datos);
+
+//        Top 10 libros mas desargados
+        System.out.println("Top 10 libros mas desargados");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
+                .limit(10)
+                .map(l->l.titulo().toUpperCase())
+                .forEach(System.out::println);
 
     }
 }
